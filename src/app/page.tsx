758535c059ae564c5a8e04b527519173ca7065ea
assembouchekifa <147662333+assembouchekifa@users.noreply.github.com>
2024-03-14 16:07:43 +0100

    location donn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,119 @@
 "use client";
 
 import Card from "@/components/Card";
-import { useEffect } from "react";
+import Timingpreay from "@/components/Timingpreay";
+import { useEffect, useState } from "react";
 import { FaMagnifyingGlassLocation } from "react-icons/fa6";
+import { IoMdRemoveCircleOutline } from "react-icons/io";
 
 function Home() {
+  let locati: {
+    latitude: number;
+    longitude: number;
+  };
+  const [log, setLog] = useState<boolean>(false);
+  const [city, setCity] = useState<{
+    countryName: string;
+    city: string;
+  }>();
+
   function hundelclic() {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(
-        position.coords.latitude + " and " + position.coords.longitude
-      );
-    }),
-      () => {
-        console.log("use manual");
+      const local = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
       };
+      localStorage.setItem("local", JSON.stringify(local));
+      locati = { latitude: local.latitude, longitude: local.longitude };
+      setLog(true);
+      getLocation();
+    });
   }
 
+  async function getLocation() {
+    const data = await fetch(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${locati?.latitude}&longitude=${locati?.longitude}&localityLanguage=en`
+    )
+      .then((data) => {
+        return data.json();
+      })
+      .catch((e) => {
+        console.error("something wrong");
+      });
+    if (data.city && data.countryName) {
+      setCity({
+        city: data.city,
+        countryName: data.countryName,
+      });
+    } else {
+      console.error("something wrong");
+    }
+  }
   useEffect(() => {
-    let a = setInterval(() => {
-      let date = new Date();
-      console.log(date.getSeconds());
-    }, 1000);
-    return () => {
-      clearInterval(a);
-    };
+    let local = localStorage.getItem("local");
+    if (local) {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      locati = JSON.parse(local);
+      setLog(true);
+    } else {
+      locati = {
+        latitude: 21.42251,
+        longitude: 39.826168,
+      };
+      setLog(false);
+    }
+    getLocation();
   }, []);
 
+  useEffect(() => {}, [city]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center sm:p-24 p-12 md:p-32 ">
+    <main className="flex min-h-screen flex-col items-center justify-center sm:p-24 p-12 md:p-32 text-3xl">
       <div
         style={{ backgroundColor: "#fafafa99" }}
         className="w-full  pt-1 rounded-md"
       >
-        <h1 style={{ letterSpacing: "0.5px" }} className=" font-bold mb-1 ">
+        <h1
+          style={{ letterSpacing: "0.5px" }}
+          className="ps-2 mb-2 font-bold  "
+        >
           Prayer Timings
         </h1>
-        <div>Location :{/* {contry} {city}*/}</div>
-
-        <div className="w-full flex items-center justify-center px-4 py-1 mb-1 ">
-          <span className="font-bold me-1">{"fajre"}</span> prayer :{" "}
-          <span className="font-bold">{"-02:30"}</span>{" "}
+        <div className="ps-2 mb-2">
+          Location :{city?.countryName} {city?.city}
         </div>
-        <div className="text-end text-sm pe-2">2024/78/95</div>
+        <Timingpreay />
+        <div className="text-end text-xl pe-2">2024/78/95</div>
         <Card salat="fajre" time="4:50" border={true} />
         <Card salat="fajre" time="4:50" border={true} />
         <Card salat="fajre" time="4:50" border={true} />
         <Card salat="fajre" time="4:50" border={true} />
         <Card salat="fajre" time="4:50" border={false} />
-        <button
-          className="text-xs flex items-center  my-2 justify-center"
-          onClick={hundelclic}
-        >
-          Select location automatically
-          <FaMagnifyingGlassLocation className="ms-1" />
-        </button>
+        {!log ? (
+          <button
+            className="text-lg flex items-center w-full text-center my-2 justify-center"
+            onClick={hundelclic}
+          >
+            Select location automatically
+            <FaMagnifyingGlassLocation className="ms-1" />
+          </button>
+        ) : (
+          <button
+            className="text-lg flex items-center w-full text-center  my-2 justify-center"
+            onClick={() => {
+              locati = {
+                latitude: 21.42251,
+                longitude: 39.826168,
+              };
+              localStorage.clear();
+              getLocation();
+              setLog(false);
+            }}
+          >
+            Remove location
+            <IoMdRemoveCircleOutline className="ms-1" />
+          </button>
+        )}
       </div>
     </main>
   );
@@ -61,6 +121,4 @@ function Home() {
 
 export default Home;
 
-// https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=XXXXXXXXXXXX&longitude=XXXXXXXXXXXX&localityLanguage=en
 // https://api.aladhan.com/v1/calendar/2024/3?latitude=35.6941824&longitude=-0.6782976&method=2
-// https://api.aladhan.com/v1/calendarByCity/2024/3?city=Oran&country=DZ&method=2
---
+++ src/components/Timingpreay.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+function Timingpreay() {
+  const [time, setTime] = useState<number>();
+
+  useEffect(() => {
+    let a = setInterval(() => {
+      let date = new Date();
+      setTime(date.getTime());
+    }, 1000);
+    return () => {
+      clearInterval(a);
+    };
+  }, []);
+  return (
+    <div className="w-full flex items-center justify-center px-4 py-1 mb-1 ">
+      <span className="font-bold me-1">{"fajre"}</span> prayer :{" "}
+      <span className="font-bold">{"-02:30"}</span>{" "}
+    </div>
+  );
+}
+
+export default Timingpreay;
