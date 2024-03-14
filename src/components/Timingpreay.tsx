09b2d0efb9dab264ee93bc36e3b52b474c4627db
assembouchekifa <147662333+assembouchekifa@users.noreply.github.com>
2024-03-14 23:00:16 +0100

    donnnn

--- src/components/Timingpreay.tsx
+++ src/components/Timingpreay.tsx
@@ -56,7 +56,7 @@ function Timingpreay({ cal }: any) {
     return () => {
       clearInterval(a);
     };
-  }, []);
+  }, [cal]);
   return (
     <div className="w-full flex items-center justify-center px-4 py-1 mb-1 ">
       <span className="font-bold me-1">{preay}</span> prayer :{" "}
